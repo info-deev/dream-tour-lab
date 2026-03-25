168c24f63fcc9853184c2ea538f81c92ad1002da
Alexander Deev <info.deev@yandex.ru>
2026-03-25 14:18:40 +0500

    yml update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
-    baseURL: "/dream-tour-lab/",
     head: {
       title: "Дрим тур", // default fallback title
       htmlAttrs: {
